e3354cef7425d8e79dd8ec2071162b022c8684da
s19685 <s19685@pjwstk.edu.pl>
2020-04-16 23:53:21 +0200

    some skeleton

--- MPP03_S19685/src/com/company/Main.java
+++ MPP03_S19685/src/com/company/Main.java
@@ -1,8 +1,43 @@
 package com.company;
 
+import java.io.File;
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+
+        List<Preceptron> preceptrons = new ArrayList();
+        File file = new File("data");
+        String[] directories = file.list((x, d) -> new File(x, d).isDirectory());
+        String options = "";
+
+        for (String lang : directories) {
+            preceptrons.add(new Preceptron(lang));
+            System.out.println(lang);
+            options += lang + " ";
+        }
+
+
+        while (true) {
+            String test = "";
+            System.out.println("Wpisz tekst w jednym z jezykow: " + options);
+
+            Scanner scanner = new Scanner(System.in);
+            while (scanner.hasNextLine()) {
+                test += scanner.nextLine();
+                System.out.println(test);
+                //if(scanner.nextLine().equals("xd")) System.out.println("xd");
+
+                //ZROB test dla wczytanego testu
+                //PODAJ WYNIK TESTU
+            }
+
+            System.out.println(test);
+        }
     }
 }
---
+++ MPP03_S19685/src/com/company/Preceptron.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Preceptron {
+    private String name;
+
+
+    public Preceptron(String name){
+        this.name = name;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Preceptron{"+ name + "}";
+    }
+
+}
