b286739456057bcf7ae89e24c4e6f336355f5960
s19685 <s19685@pjwstk.edu.pl>
2020-05-11 03:10:26 +0200

    almost done

--- MPP05_S19685/src/zad1/Bayes.java
+++ MPP05_S19685/src/zad1/Bayes.java
@@ -5,101 +5,92 @@ import java.util.List;
 
 public class Bayes {
 
-    private List<String[]> versicolor;
-    private List<String[]> virginica;
-    private List<String[]> setosa;
-    private int dataSize;
-    private List<String[]> training;
+    private List<double[]> SETOSAs = new ArrayList<>();
+    private List<double[]> VIRGINICAs = new ArrayList<>();
+    private List<double[]> VERSICOLORs = new ArrayList<>();
 
-    private static double[] x1;
-    private static double[] x2;
-    private static double[] x3;
-    private static double[] x4;
-
-    private List<double[]> vectors;
-
-    public Bayes(List<String[]> training ,List<String[]> test ){
-        this.training = training;
-        vectors = new ArrayList<>();
-        setosa = new ArrayList<>();
-        virginica = new ArrayList<>();
-        versicolor = new ArrayList<>();
-        x1 = new double[training.size()];
-        x2 = new double[training.size()];
-        x3 = new double[training.size()];
-        x4 = new double[training.size()];
-        dataSize = training.size();
-        for (int i = 0; i <dataSize ; i++) {
-
-            x1[i] = Double.valueOf(training.get(i)[0].replace(",","."));
-            x2[i] = Double.valueOf(training.get(i)[1].replace(",","."));
-            x3[i] = Double.valueOf(training.get(i)[2].replace(",","."));
-            x4[i] = Double.valueOf(training.get(i)[3].replace(",","."));
-            vectors.add(new double[]{x1[i], x2[i], x3[i], x4[i]});
-            if(training.get(i)[4].equals("Iris-setosa")) setosa.add(training.get(i));
-            if(training.get(i)[4].equals("Iris-virginica")) virginica.add(training.get(i));
-            if(training.get(i)[4].equals("Iris-versicolor")) versicolor.add(training.get(i));
+    public Bayes(List<String[]> training) {
+        for (int i = 0; i < Main.SIZE; i++) {
+            SETOSAs.add(new double[40]);
+            VIRGINICAs.add(new double[40]);
+            VERSICOLORs.add(new double[40]);
         }
-        int it = 0;
-        for (double[] vector : vectors ) {
-            System.out.println(countProbability(vector,training.get(it)[4]));
-            it++;
+
+        int index = 0;
+        for (String[] line : training) {
+            for (int i = 0; i < Main.SIZE; i++) {
+                if(line[4].equals("Iris-setosa")) SETOSAs.get(i)[index] = Double.valueOf(line[i].replace(",", "."));
+                if(line[4].equals("Iris-virginica")) VIRGINICAs.get(i)[index] = Double.valueOf(line[i].replace(",","."));
+                if(line[4].equals("Iris-versicolor")) VERSICOLORs.get(i)[index] = Double.valueOf(line[i].replace(",","."));
+            }
+            index = index == 39 ? 0 : index + 1;
         }
     }
 
-
-    public static int findAllfor(double value, int column){
-        int result = 0;
-
-        switch (column){
-            case 0:
-                for (double d: x1 ) if(d == value) result++;
-                break;
-            case 1:
-                for (double d: x2 ) if(d == value) result++;
-                break;
-            case 2:
-                for (double d: x3 ) if(d == value) result++;
-                break;
-            case 3:
-                for (double d: x4 ) if(d == value) result++;
-                break;
+    public void testing(List<String[]> test) {
+        int counter = 0;
+        int sum = test.size();
+        for (String[] line : test ) {
+            double[] viktor = new double[4];
+            for (int i = 0; i < viktor.length; i++) viktor[i] = Double.valueOf(line[i].replace(",","."));
+            if(classify(viktor).equals(line[4])) counter++;
         }
 
+        System.out.println("Poprawnie zaklasyfikowano "+String.format("%.2f",100.0*counter/sum)+"%");
+    }
+
+    public String classify(double[] vector) {
+        double setosa = countProbability(vector, "Iris-setosa");
+        double virginica = countProbability(vector, "Iris-virginica");
+        double versicolor = countProbability(vector, "Iris-versicolor");
+        System.out.println("\nsetosa: " + setosa);
+        System.out.println("virginica: " + virginica);
+        System.out.println("versicolor: " + versicolor);
+        double max = Math.max(setosa, Math.max(virginica, versicolor));
+
+        return max == setosa ? "Iris-setosa" : max == virginica ? "Iris-virginica" : "Iris-versicolor";
+    }
+
+
+    public double countProbability(double[] vector, String decision) {
+        double result = 1.0;
+        int all = 40;
+        System.out.println(decision + " przed wygladzaniem");
+        for (int i = 0; i < vector.length; i++) {
+            result *= howMany(vector[i], findBy(decision).get(i)) / all;
+            System.out.println(result);
+        }
+        if (result == 0.0) {
+            System.out.println("po wygladzaniu");
+            result = 1.0;
+            for (int i = 0; i < vector.length; i++) {
+                result *= (1 + howMany(vector[i], findBy(decision).get(i))) / (all + 3);
+                System.out.println(result);
+            }
+        }
+        else{
+            System.out.println("po wygladzaniu pierwszego argumentu");
+            result = (1 + howMany(vector[0], findBy(decision).get(0))) / (all + 3);
+            for (int i = 1; i < vector.length; i++) {
+                result *= howMany(vector[i], findBy(decision).get(i)) / all;
+                System.out.println(result);
+            }
+        }
+        System.out.println("probability: "+result*all/120+"\n");
+        return result / 3;
+    }
+
+    public double howMany(double d, double[] from) {
+        double result = 0.0;
+        for (double ds : from) if (d == ds) result++;
         return result;
     }
 
-    public double countProbability(double[] vector, String decision){
-        double result =0.0;
-
-        int all = findBy(decision).size();
-        for (int i =0; i<vector.length; i++) {
-            result *= findfor(vector[i],i,findBy(decision))/all;
-        }
-        System.out.println(result);
-        result *= 0.3333;
-        return result;
-    }
-
-    private double findfor(double d,int index, List<String[]> from) {
-        double result =0.0;
-        for (String[] line : from) {
-            if(Double.valueOf(line[index].replace(",",".")) == d) result++;
-        }
-        return result;
-
-    }
-
-    private List<String[]> findBy(String id) {
-        if(id.equals("Iris-setosa")) return setosa;
-        if(id.equals("Iris-virginica")) return virginica;
-        if(id.equals("Iris-versicolor")) return versicolor;
+    private List<double[]> findBy(String id) {
+        if (id.equals("Iris-setosa")) return SETOSAs;
+        if (id.equals("Iris-virginica")) return VIRGINICAs;
+        if (id.equals("Iris-versicolor")) return VERSICOLORs;
         return null;
     }
 
-
-    @Override
-    public String toString() {
-        return "setosa = "+setosa + " virginica = "+ virginica + " versicolor = "+ versicolor;
-    }
 }
--- MPP05_S19685/src/zad1/Main.java
+++ MPP05_S19685/src/zad1/Main.java
@@ -1,22 +1,20 @@
 package zad1;
 
-import java.awt.*;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-import java.util.concurrent.PriorityBlockingQueue;
 
 public class Main {
 
-    private static final int SIZE = 4;
+    static final int SIZE = 4;
     private static List<String[]> TRAINING_SET;
     private static List<String[]> TEST_SET;
 
     public static void main(String[] args) {
-	// write your code here
+
         TRAINING_SET = new ArrayList<>();
         TEST_SET = new ArrayList<>();
 
@@ -28,35 +26,20 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        training();
-        testing();
 
-        Bayes bayes = new Bayes(TRAINING_SET,TEST_SET);
-
-        System.out.println(Bayes.findAllfor(0.2,3));
-        System.out.println(bayes);
+        Bayes bayes = new Bayes(TRAINING_SET);
+        bayes.testing(TEST_SET);
 
         Scanner scanner = new Scanner(System.in);
-
         while (true){
 
             double [] vector = new double[SIZE];
-            System.out.println("Podaj wektor skladajacy sie z " + SIZE + "argumentow:");
+            System.out.println("\nPodaj wektor skladajacy sie z " + SIZE + "argumentow:");
             for (int i = 0; i < SIZE ; i++) {
                 System.out.println("podaj " + (i + 1) + " argument");
-                vector[i] = scanner.nextDouble(); }
-            bayesClassifier(vector);
+                vector[i] = scanner.nextDouble();
+            }
+            System.out.println("\nPodany wektor zaklasyfikowano jako: "+bayes.classify(vector));
         }
-    }
-
-    private static void testing() {
-    }
-
-    private static void training() {
-
-    }
-
-    private static void bayesClassifier(double[] vector) {
-
     }
 }
